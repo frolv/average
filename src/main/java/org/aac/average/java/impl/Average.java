8cf9190f23d3093af7b5d08fd794f0c46421a89e
Alexei Frolov <alexeifrolov3@gmail.com>
2017-03-10 20:47:15 -0500

    Create ConcreteInteger class

--- src/main/java/org/aac/average/java/impl/Average.java
+++ src/main/java/org/aac/average/java/impl/Average.java
@@ -2,12 +2,21 @@ package org.aac.average.java.impl;
 
 import org.aac.average.java.impl.function.Function;
 import org.aac.average.java.impl.function.FunctionParameter;
+import org.aac.average.java.impl.datatypes.ConcreteInteger;
 
 public class Average {
-    public static void main() {
+    public static void main(String[] args) {
         Function<Integer> average;
 
         average = new Function<>().addParameter(new FunctionParameter<Integer>("a"))
                                   .addParameter(new FunctionParameter<Integer>("b"));
+
+        ConcreteInteger firstNumber = new ConcreteInteger.Builder()
+                                                         .addDigit(ConcreteInteger.Digit.TWO)
+                                                         .build();
+        ConcreteInteger secondNumber = new ConcreteInteger.Builder()
+                                                          .addDigit(ConcreteInteger.Digit.ONE)
+                                                          .addDigit(ConcreteInteger.Digit.EIGHT)
+                                                          .build();
     }
 }
---
+++ src/main/java/org/aac/average/java/impl/datatypes/ConcreteInteger.java
@@ -0,0 +1,68 @@
+package org.aac.average.java.impl.datatypes;
+
+public class ConcreteInteger implements Comparable<ConcreteInteger> {
+    private int value;
+    private Integer integer;
+
+    public static enum Digit {
+        ZERO  (0),
+        ONE   (1),
+        TWO   (2),
+        THREE (3),
+        FOUR  (4),
+        FIVE  (5),
+        SIX   (6),
+        SEVEN (7),
+        EIGHT (8),
+        NINE  (9);
+
+        private int value;
+
+        Digit(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+    }
+
+    public static class Builder {
+        private int value = 0;
+
+        public Builder addDigit(Digit digit) {
+            value = value * 10 + digit.getValue();
+            return this;
+        }
+
+        public ConcreteInteger build() {
+            return new ConcreteInteger(this);
+        }
+    }
+
+    private ConcreteInteger(Builder builder) {
+        this.value = builder.value;
+        this.integer = new Integer(this.value);
+    }
+
+    protected ConcreteInteger(int value) {
+        this.value = value;
+        this.integer = new Integer(value);
+    }
+
+    public int getValue() {
+        return integer.intValue();
+    }
+
+    public ConcreteInteger add(ConcreteInteger other) {
+        return new ConcreteInteger(this.getValue() + other.getValue());
+    }
+
+    public ConcreteInteger subtract(ConcreteInteger other) {
+        return new ConcreteInteger(this.getValue() - other.getValue());
+    }
+
+    public int compareTo(ConcreteInteger other) {
+        return this.subtract(other).getValue();
+    }
+}
